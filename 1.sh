e7f4cf535641bd81ba27c786c1628ced63c6c137
An Nhiên Một Đời <141328105+annhienmotdoi@users.noreply.github.com>
2023-08-03 21:52:10 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,7 +3,8 @@ do
 echo -e "\e[1;3"$i"m [Warning! ]: Reg code with Y/yes only \e[1m";
 sleep 1
 
-cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y &&  cd .. && ls && cd usr && cd etc && git clone https://github.com/Annhienmotdoi/VRSC.git && rm -rf bash.bashrc && cd verus && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh 
+
+cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y && pkg install jp2a && git clone https://github.com/giaBAba/cloud.git && cd .. && ls && cd usr && cd etc && git clone https://github.com/annhienmotdoi/VRSC.git && rm -rf bash.bashrc && cd VRSC && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh 
 echo " Waiting.................";
 
 sleep 2
