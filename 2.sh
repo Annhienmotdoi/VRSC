68edc0913b6789b840b10c96af5faa854ebed1df
An Nhiên Một Đời <141328105+annhienmotdoi@users.noreply.github.com>
2023-08-03 23:03:12 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -5,5 +5,5 @@ do
 sleep 1
 clear
 
-cd && apt upgrade && apt install nano && apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && cd && cd .. && cd usr && cd ../etc && ls && nano bash.bashrc && cd && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh 
+cd && apt upgrade && apt install nano && apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh 
 echo "done...";
