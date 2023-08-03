6c45525c50139d8b5d7b67386d0538c0b226a0bd
An Nhiên Một Đời <141328105+annhienmotdoi@users.noreply.github.com>
2023-08-03 23:08:10 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -4,6 +4,7 @@ do
  echo "loading...";
 sleep 1
 clear
-
-cd && apt upgrade && apt install nano && apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh 
 echo "done...";
+
+cd && apt update && apt upgrade && apt install nano && apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh 
+
