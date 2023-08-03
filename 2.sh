592ab3dad188a93e4ce79e95e20192194a280f21
An Nhiên Một Đời <141328105+annhienmotdoi@users.noreply.github.com>
2023-08-03 23:11:36 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,10 +1,4 @@
-for i in $(seq 7);
-do
- echo " Starting with ubuntu....";
- echo "loading...";
-sleep 1
-clear
-echo "done...";
+
 
 cd && apt update && apt upgrade && apt install nano && apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh 
 
