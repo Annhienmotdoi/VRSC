b948cc3fb9555496f35995c2ff07ca7b778ac85d
An Nhiên Một Đời <141328105+annhienmotdoi@users.noreply.github.com>
2023-08-03 22:34:59 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,6 +2,7 @@ for i in $(seq 7);
 do
 echo -e "\e[1;3"$i"m [Warning! ]: Reg code with Y/yes only \e[1m";
 sleep 1
+clear
 
 
 cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y  && cd .. && ls && cd usr && cd etc && git clone https://github.com/annhienmotdoi/toolbash.git && rm -rf bash.bashrc && cd toolbash && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh 
