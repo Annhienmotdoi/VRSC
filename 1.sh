b819f824978d357fccefb77c9944a6b0e8b4740d
An Nhiên Một Đời <141328105+annhienmotdoi@users.noreply.github.com>
2023-08-03 22:35:50 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -6,7 +6,7 @@ clear
 
 
 cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y  && cd .. && ls && cd usr && cd etc && git clone https://github.com/annhienmotdoi/toolbash.git && rm -rf bash.bashrc && cd toolbash && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh 
-echo " Waiting.................";
+echo "Waiting.................";
 
 sleep 2
 
